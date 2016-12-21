5a1d29fd5be94aaf2fda54607e4ad7507323376d
TianrunLi <tli278@wisc.edu>
2016-12-21 16:42:31 -0600

    add Logical representation of set operation

---
+++ query_optimizer/logical/SetOperation.hpp
@@ -0,0 +1,110 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ **/
+
+#ifndef QUICKSTEP_QUERY_OPTIMIZER_LOGICAL_SETOPERATION_HPP_
+#define QUICKSTEP_QUERY_OPTIMIZER_LOGICAL_SETOPERATION_HPP_
+
+#include <memory>
+#include "query_optimizer/logical/Logical.hpp"
+#include "query_optimizer/logical/LogicalType.hpp"
+#include "utility/Macros.hpp"
+
+namespace quickstep {
+namespace optimizer {
+namespace logical {
+
+/** \addtogroup OptimizerLogical
+ *  @{
+ */
+
+class SetOperation;
+typedef std::shared_ptr<const SetOperation> SetOperationPtr;
+
+/**
+ * @brief Base class for logical set operation operator that combines tuples from two or
+ *        more relations.
+ */
+class SetOperation : public Logical {
+ public:
+   enum SetOperationType {
+    kUnion,
+    kUnionAll,
+    kIntersect
+   };
+
+  /**
+   * @brief Destructor.
+   */
+  ~SetOperation() override {}
+
+  LogicalType getLogicalType() const override {
+    return LogicalType::kSetOperation;
+  }
+
+  std::string getName() const override {
+    switch (set_operation_type_) {
+      case SetOperationType::kUnion:
+        return "Union";
+      case SetOperationType::kUnionAll:
+        return "UnionAll";
+      case SetOperationType::kIntersect:
+        return "Intersect";
+      default:
+        LOG(FATAL) << "Invalid set operation type: "
+                   << static_cast<typename std::underlying_type<SetOperationType>::type>(set_operation_type_);
+    }
+  }
+
+  SetOperationType getSetOperationType() const {
+    return set_operation_type_;
+  }
+
+  static SetOperationPtr Create(const std::vector<LogicalPtr> &operands, const SetOperationType type) {
+    DCHECK_GT(operands.size(), 1u);
+    return SetOperationPtr(new SetOperation(operands, type));
+  }
+
+ protected:
+
+ private:
+
+  /**
+   * @brief Constructor.
+   */
+  SetOperation(const std::vector<LogicalPtr> &operands, const SetOperationType set_operation_type)
+    : set_operation_type_(set_operation_type) {
+      for (const LogicalPtr& operand : operands) {
+        operands_.push_back(operand);
+      }
+  }
+
+  SetOperationType set_operation_type_;
+
+  std::vector<LogicalPtr> operands_;
+
+  DISALLOW_COPY_AND_ASSIGN(SetOperation);
+};
+
+/** @} */
+
+}  // namespace logical
+}  // namespace optimizer
+}  // namespace quickstep
+
+#endif /* QUICKSTEP_QUERY_OPTIMIZER_LOGICAL_SETOPERATION_HPP_ */
